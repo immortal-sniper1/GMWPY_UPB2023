63dfd1d09ffca2dac32b80dc6f01b39aaf8c88bd
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2023-03-08 12:21:33 +0200

    f

---
+++ SW/ADC_reader/ADC_reader.ino
@@ -0,0 +1,136 @@
+#include <Wire.h>
+#include <MCP342x.h>
+
+// 0x68 is the default address for all MCP342x devices
+uint8_t address1 = 0xA0;
+MCP342x adc1 = MCP342x(address1);
+uint8_t address2 = 0xA3;
+MCP342x adc2 = MCP342x(address2);
+
+/*
+ADC1:
+CH1 CH4 REF PIN
+CH2 CH4 GAS PIN
+
+ADC2:
+CH1 OUTPUT WITH THE REFERENCE ON THE - SIDE
+CH2 REFERENCE MEASURED FROM GND
+
+*/
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  // Enable power for MCP342x (needed for FL100 shield only)
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
+
+  // Reset devices
+  MCP342x::generalCallReset();
+  delay(1); // MC342x needs 300us to settle, wait 1ms
+
+  // Check device present
+  Wire.requestFrom(address1, (uint8_t)1);
+  if (!Wire.available()) {
+    Serial.print("No device found at address ");
+    Serial.println(address1, HEX);
+    while (1)
+      ;
+  }
+
+}
+
+void loop(void)
+{
+  long value1 = 0;
+  long value2 = 0;
+  long value3 = 0;
+  long value4 = 0;
+    uint8_t err1,err2;
+
+  MCP342x::Config status;
+
+  // Initiate a conversion; convertAndRead() will wait until it can be read
+   err1 = adc1.convertAndRead(MCP342x::channel1, MCP342x::oneShot,
+                                    MCP342x::resolution18, MCP342x::gain1,
+                                    1000000, value1, status);
+
+
+
+  // Initiate a conversion; convertAndRead() will wait until it can be read
+   err2 = adc2.convertAndRead(MCP342x::channel1, MCP342x::oneShot,
+                                    MCP342x::resolution18, MCP342x::gain1,
+                                    1000000, value3, status);
+
+  if (err1)
+  {
+    Serial.print("Convert error: ");
+    Serial.println(err1);
+  }
+  else
+  {
+    Serial.print("Value1: ");
+    Serial.println(value1);
+  }
+
+  
+    if (err2)
+  {
+    Serial.print("Convert error: ");
+    Serial.println(err2);
+  }
+  else
+  {
+    Serial.print("Value3: ");
+    Serial.println(value3);
+  }
+
+  delay(10);
+//////////////////////////////////////////
+  // Initiate a conversion; convertAndRead() will wait until it can be read
+   err1 = adc1.convertAndRead(MCP342x::channel2, MCP342x::oneShot,
+                                    MCP342x::resolution18, MCP342x::gain1,
+                                    1000000, value2, status);
+
+
+
+  // Initiate a conversion; convertAndRead() will wait until it can be read
+   err2 = adc2.convertAndRead(MCP342x::channel2, MCP342x::oneShot,
+                                    MCP342x::resolution18, MCP342x::gain1,
+                                    1000000, value4, status);
+
+  if (err1)
+  {
+    Serial.print("Convert error: ");
+    Serial.println(err1);
+  }
+  else
+  {
+    Serial.print("Value1: ");
+    Serial.println(value2);
+  }
+
+  
+    if (err2)
+  {
+    Serial.print("Convert error: ");
+    Serial.println(err2);
+  }
+  else
+  {
+    Serial.print("Value3: ");
+    Serial.println(value4);
+  }
+
+
+
+
+  
+  delay(10);
+  delay(1000);
+}
+
+
---
+++ SW/MCP3424-master/README.md
@@ -0,0 +1,35 @@
+MCP3424
+=======
+
+library for the Arduino.
+
+  The MCP3424 is a low-cost 18Bit Sigma-Delta ADC with I2C Interface.
+
+  This library implements the following features:
+
+   - channel setting by address or pin selection
+   - continuous and one_shot modes
+   - selectable sample rates 12,14,16 and 18 bit
+   - selectable PGA gains: x1, x2, x4, x8
+   - error detection: overflow, underflow, i2c, progress, timeout
+   - selectable blocking/non-blocking modes
+   - highest valid gain selection
+   - general call signalling            
+
+
+  Author:          Bernhard Schneider <bernhard@neaptide.org><br>
+  Available from:  https://github.com/bersch/MCP3424<br>
+  License:         GNU LGPL<br>
+
+  
+  This library may also be used for MCP3421, MCP3422, MCP3423, MCP3425, MCP3426, MCP3427, MCP3428
+
+  #### See also: 
+  
+  ADC | Link
+  ------------ | -------------
+  **MCP3421** | http://ww1.microchip.com/downloads/en/DeviceDoc/22003e.pdf<br>
+  **MCP3422/3/4** | http://ww1.microchip.com/downloads/en/DeviceDoc/22088b.pdf<br>
+  **MCP3425** | http://ww1.microchip.com/downloads/en/DeviceDoc/22072b.pdf<br>
+  **MCP3426/7/8** | http://ww1.microchip.com/downloads/en/DeviceDoc/22226a.pdf<br>
+  
