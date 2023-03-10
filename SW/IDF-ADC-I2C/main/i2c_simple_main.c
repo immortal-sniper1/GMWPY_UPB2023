af196b9253f1b0221b7be00667e181dccca7cad0
immortal_sniper1 <38986383+immortal-sniper1@users.noreply.github.com>
2023-03-10 14:17:28 +0200

    Update i2c_simple_main.c

--- SW/IDF-ADC-I2C/main/i2c_simple_main.c
+++ SW/IDF-ADC-I2C/main/i2c_simple_main.c
@@ -1,21 +1,21 @@
 /* i2c - Simple example
 
-   Simple I2C example that shows how to initialize I2C
-   as well as reading and writing from and to registers for a sensor connected over I2C.
+ Simple I2C example that shows how to initialize I2C
+ as well as reading and writing from and to registers for a sensor connected over I2C.
 
-   The sensor used in this example is a MPU9250 inertial measurement unit.
+ The sensor used in this example is a MPU9250 inertial measurement unit.
 
-   For other examples please check:
-   https://github.com/espressif/esp-idf/tree/master/examples
+ For other examples please check:
+ https://github.com/espressif/esp-idf/tree/master/examples
 
-   See README.md file to get detailed usage of this example.
+ See README.md file to get detailed usage of this example.
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+ This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+ Unless required by applicable law or agreed to in writing, this
+ software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ CONDITIONS OF ANY KIND, either express or implied.
+ */
 #include <stdio.h>
 #include "esp_log.h"
 #include "driver/i2c.h"
@@ -39,50 +39,58 @@ static const char *TAG = "i2c-simple-example";
 /**
  * @brief Read a sequence of bytes from a MPU9250 sensor registers
  */
-static esp_err_t mpu9250_register_read(uint8_t reg_addr, uint8_t *data, size_t len)
-{
-    return i2c_master_write_read_device(I2C_MASTER_NUM, MPU9250_SENSOR_ADDR, &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-}
+static esp_err_t mpu9250_register_read(uint8_t reg_addr, uint8_t *data,
+	size_t len)
+    {
+    return i2c_master_write_read_device(I2C_MASTER_NUM, MPU9250_SENSOR_ADDR,
+	    &reg_addr, 1, data, len, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    }
 
 /**
  * @brief Write a byte to a MPU9250 sensor register
  */
 static esp_err_t mpu9250_register_write_byte(uint8_t reg_addr, uint8_t data)
-{
+    {
     int ret;
-    uint8_t write_buf[2] = {reg_addr, data};
+    uint8_t write_buf[2] =
+	{
+	reg_addr, data
+	};
 
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU9250_SENSOR_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU9250_SENSOR_ADDR,
+	    write_buf, sizeof(write_buf),
+	    I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
 
     return ret;
-}
+    }
 
 /**
  * @brief i2c master initialization
  */
 static esp_err_t i2c_master_init(void)
-{
-    int i2c_master_port = I2C_MASTER_NUM;
+    {
+    int i2c_master_port1 = I2C_MASTER_NUM;
 
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-       // .sda_io_num = I2C_MASTER_SDA_IO,
-       // .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_io_num = GPIO_NUM_6,
-        .scl_io_num = GPIO_NUM_5,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
-    };
+    i2c_config_t conf =
+	{
+	.mode = I2C_MODE_MASTER,
+	// .sda_io_num = I2C_MASTER_SDA_IO,
+	// .scl_io_num = I2C_MASTER_SCL_IO,
+		.sda_io_num = GPIO_NUM_6,
+		.scl_io_num = GPIO_NUM_5,
+		.sda_pullup_en = GPIO_PULLUP_ENABLE,
+		.scl_pullup_en = GPIO_PULLUP_ENABLE,
+		.master.clk_speed = I2C_MASTER_FREQ_HZ,
+	};
 
-    i2c_param_config(i2c_master_port, &conf);
+    i2c_param_config(i2c_master_port1, &conf);
 
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-}
-
+    return i2c_driver_install(i2c_master_port1, conf.mode,
+	    I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    }
 
 void app_main(void)
-{
+    {
     uint8_t data[2];
     ESP_ERROR_CHECK(i2c_master_init());
     ESP_LOGI(TAG, "I2C initialized successfully");
@@ -92,8 +100,9 @@ void app_main(void)
     ESP_LOGI(TAG, "WHO_AM_I = %X", data[0]);
 
     /* Demonstrate writing by reseting the MPU9250 */
-    ESP_ERROR_CHECK(mpu9250_register_write_byte(MPU9250_PWR_MGMT_1_REG_ADDR, 1 << MPU9250_RESET_BIT));
+    ESP_ERROR_CHECK(
+	    mpu9250_register_write_byte(MPU9250_PWR_MGMT_1_REG_ADDR, 1 << MPU9250_RESET_BIT));
 
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
     ESP_LOGI(TAG, "I2C de-initialized successfully");
-}
+    }
